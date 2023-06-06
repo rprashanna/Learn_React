e04c546ca02023fe73045ca5f5295aae0b3fc713
PRASHANNARAJ <rprashannaraj@gmail.com>
2023-06-06 14:28:11 +0530

    MERGE

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          dit <code>Hi Raj</code> and save to reload.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
